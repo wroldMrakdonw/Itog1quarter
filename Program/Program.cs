22a7f6495899e7a29bba8b8710e51a2299904d93
wrold <tema_akim@mail.ru>
2022-11-04 19:33:01 +0400

    Метод PrintArray + осн. часть

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,16 +1,27 @@
 ﻿string[] LessThanThreeSymbols(string[] array){
-    resultLen = 0;
+    int resultLen = 0;
     for (int i = 0; i < array.Length; i++)
     {
         if(array[i].Length !> 3) resultLen++;
     }
-    result = new string[resultLen];
+    string[] result = new string[resultLen];
     int c = 0;
-    for (int i = 0; i < array.Length; i++)
+    foreach (string item in array)
     {
-        if(array[i].Length !> 3){
-            array[c] = array[i];
+        if(item.Length !> 3){
+            result[c] = item;
             c++;
         }
     }
+    return result;
 }
+
+void PrintArray(string[] array){
+    foreach (string item in array)
+    {
+        System.Console.Write($"{item} ");
+    }
+}
+
+string[] array = {"sas", "ds ", "21as", " ww a"};
+PrintArray(LessThanThreeSymbols(array));
