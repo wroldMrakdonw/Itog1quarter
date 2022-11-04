f805065fe0ef1b3639819b8d08530c3c9cf58929
wrold <tema_akim@mail.ru>
2022-11-04 19:21:30 +0400

    Метод для выполнения программы

---
+++ Program/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] LessThanThreeSymbols(string[] array){
+    resultLen = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length !> 3) resultLen++;
+    }
+    result = new string[resultLen];
+    int c = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length !> 3){
+            array[c] = array[i];
+            c++;
+        }
+    }
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-# Itog1quarter
